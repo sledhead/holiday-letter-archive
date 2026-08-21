c75c411cdd5e82de516c50f73a5cb375d25b72bf
sledhead <bigskyprogrammers@gmail.com>
2026-08-21 02:00:56 -0600

    updated setting files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/holiday-letter-archive/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
